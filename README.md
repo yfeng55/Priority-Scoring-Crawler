8b890f1f17da29a56a8d2eefe50ad7c7d38abd6a
Evan Feng <evanfeng55@gmail.com>
2016-03-17 01:45:36 -0400

    get user input from provided arguments

---
+++ README.md
@@ -0,0 +1,22 @@
+
+Get Input from User ***
+
+    (-u) A URL from which to start the crawl (presumably, a web page that deals with the subject)
+    (-q) A query: a set of words, possibly null
+    (-docs) A path name for a directory to save the downloaded pages
+    (-m) A maximum number of pages to download. This should default to 50
+    (-t) A flag for generating a trace. This defaults to false
+
+    example:
+    java crawler -u https://en.wikipedia.org/wiki/Cleopatra -q Cleopatra Caesar Anthony -docs ~/myaccount/cleopatra/ -m 300
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
--- src/com/yf833/Main.java
+++ src/com/yf833/Main.java
@@ -3,19 +3,68 @@ package com.yf833;
 // A minimal Web Crawler written in Java
 // Usage: From command line
 //     java WebCrawler <URL> [N]
+// ex: http://cs.nyu.edu/courses/spring16/CSCI-GA.2580-001/MarineMammal/
+
 //  where URL is the url to start the crawl, and N (optional)
 //  is the maximum number of pages to download.
 
 
 public class Main {
 
+    private static String url_input;                // -u
+    private static String path_input;               // -docs
+    private static String query_input = "";         // -q
+    private static int maxPages = 50;               // -m
+    private static boolean showTrace = false;       // -t
+
+
 
     public static void main(String[] args){
 
-        WebCrawler wc = new WebCrawler();
-        wc.run(args);
+        //get input values from string arguments
+        getInput(args);
 
 
+
+//        WebCrawler wc = new WebCrawler();
+//        wc.run(args);
+
+
+    }
+
+
+
+    private static void getInput(String[] args){
+        for(int i=0; i<args.length; i++){
+            switch(args[i]){
+                case "-u":
+                    url_input = args[i+1];
+                    i++;
+                    break;
+                case "-docs":
+                    path_input = args[i+1];
+                    i++;
+                    break;
+                case "-q":
+                    query_input = args[i+1];
+                    i++;
+                    break;
+                case "-m":
+                    maxPages = Integer.parseInt(args[i+1]);
+                    i++;
+                    break;
+                case "-t":
+                    showTrace = true;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Must provide arguments: " + "-u <url> -q <query> -docs <path> -m <max pages> -t");
+            }
+        }
+//        System.out.println("-u: " + url_input);
+//        System.out.println("-docs: " + path_input);
+//        System.out.println("-q: " + query_input);
+//        System.out.println("-m: " + maxPages);
+//        System.out.println("-t: " + showTrace);
     }
 
 }
